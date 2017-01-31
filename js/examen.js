2c30ef44230767a1824618ece77b968ff8363c9e
JoLuRe <jordi.lucio@gmail.com>
2017-01-31 04:07:38 +0100

    Update examen.js

--- js/examen.js
+++ js/examen.js
@@ -13,8 +13,8 @@ window.onload = function(){
 //funcion donde cogemos los datos del xml y los ponemos en el html 
 function gestionarXml(dadesXml){
  var xmlDoc = dadesXml.responseXML;
- document.getElementById("q001").innerHTML = xmlDoc.getElementsByTagName("text")[0].childNodes[0].nodeValue;
+ document.getElementById("q001").innerHTML = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
  clue_1=parseInt(xmlDoc.getElementsByTagName("answer")[0].childNodes[0].nodeValue);
- document.getElementById("q002").innerHTML = xmlDoc.getElementsByTagName("text")[1].childNodes[0].nodeValue;
+ document.getElementById("q002").innerHTML = xmlDoc.getElementsByTagName("title")[1].childNodes[0].nodeValue;
  clue_2=parseInt(xmlDoc.getElementsByTagName("answer")[1].childNodes[0].nodeValue);
 }
